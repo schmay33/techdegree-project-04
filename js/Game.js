8b8fce6e1558570b8a2d8ed60c20b4f0928e2878
Nathan Schmidt <nschmdt@gmail.com>
2021-09-22 22:05:46 -0500

    Step 1 through 8

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,48 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = [new Phrase("Phrase 1"), 
+                        new Phrase("Phrase 2"), 
+                        new Phrase("Phrase 3"), 
+                        new Phrase("Phrase 4"), 
+                        new Phrase("Phrase 5")];
+        this.activePhrase = null;
+    }
+
+    /**
+     * Begins game by selecting a random phrase and displaying it to the user
+     */
+    startGame() {
+        console.log('asdf');
+        document.getElementById("overlay").style.visibility = "hidden";
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    /**
+     * Selects random phrase from phrases property
+     * @return {object} Phrase object chosen to be used
+     */
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random()*this.phrases.length)];
+    }
+
+    handleInteractions() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay() {
+        let elem = document.querySelector("#phrase ul");
+        for (let i in this.phrase) {
+            let newElem = document.createElement("li");
+            if (this.phrase[i] === " ") {
+                newElem.innerHTML = '<li class="space"> </li>';
+                elem.append(newElem);
+            } else {
+                newElem.innerHTML = `<li class="hide letter ${this.phrase[i]}">${this.phrase[i]}</li>`;
+                elem.append(newElem);
+            }
+        }
+    }
+
+    checkLetter() {
+
+    }
+
+    showMatchedLetter() {
+
+    }
+}
